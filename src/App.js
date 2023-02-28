ebc93aa89597bd7098f45c7304a7e908dd03f69c
Trần Nhất Quang <trannhatquang2405@gmail.com>
2023-02-28 11:26:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Trần Nhất Quang HELLO WORLD
         </a>
       </header>
     </div>
